7f792ad7e1972663917c555e4599c8682d34b9ac
neozaru <neozaru@mailoo.org>
2021-06-04 14:56:32 +0200

    fix: improved tested for depositWithPermit funcs

--- contracts/Mocks/MintableERC20.sol
+++ contracts/Mocks/MintableERC20.sol
@@ -3,10 +3,11 @@ pragma solidity >=0.6.12 < 0.9.0;
 // SPDX-License-Identifier: MIT
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/drafts/ERC20Permit.sol";
 
-contract MintableERC20 is ERC20 {
+contract MintableERC20 is ERC20, ERC20Permit {
 
-  constructor(string memory name, string memory symbol) public ERC20(name, symbol) {}
+  constructor(string memory name, string memory symbol) public ERC20(name, symbol) ERC20Permit(name) {}
 
   function mint(address destination, uint256 amount) public {
     _mint(destination, amount);
